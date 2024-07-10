84fa2254be4d9dcecf150d517ed171a113ec98ba
SergioG <sl_garcia@yahoo.com.ar>
2024-07-10 11:20:49 -0300

    1)Inicialización del proyecto Angular y creación del componente Saludo

---
+++ src/app/saludo/saludo.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saludo',
+  templateUrl: './saludo.component.html',
+  styleUrls: ['./saludo.component.css']
+})
+export class SaludoComponent {
+  saludo: string = '¡Hola, Angular!';
+  Nombre = 'Sergio Garcia'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
