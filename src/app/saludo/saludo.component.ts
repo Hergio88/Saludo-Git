e7fb0cc12b8b2bfe72004a44ffb96189e6a96de3
SergioG <sl_garcia@yahoo.com.ar>
2024-07-10 12:51:25 -0300

    n3.directivas-ngif

--- src/app/saludo/saludo.component.ts
+++ src/app/saludo/saludo.component.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-saludo',
   templateUrl: './saludo.component.html',
   styleUrls: ['./saludo.component.css']
 })
-export class SaludoComponent {
-  saludo: string = '¡Hola, Angular!';
+export class SaludoComponent implements OnInit {
+
+  @Input() mostrarSaludo: boolean = false;
+
   nombreYapellido: string = 'Sergio Garcia'
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 
 }
